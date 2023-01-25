65660ce1fc94e85e9986eae1c2f361dfb009edcd
gotandajidou <116865648+gotandajidou@users.noreply.github.com>
2023-01-25 23:17:04 +0900

    Create code.rb

---
+++ code.rb
@@ -0,0 +1,6 @@
+Last login: Wed Jan 25 23:10:19 on ttys000
+wangzhenyuan@wangzheuandeAir ~ % cd my_first_rails_app
+wangzhenyuan@wangzheuandeAir my_first_rails_app % heroku run rails db:migrate
+Running rails db:migrate on ⬢ agile-ridge-64162... up, run.5993 (Basic)
+wangzhenyuan@wangzheuandeAir my_first_rails_app % heroku open
+#https://agile-ridge-64162.herokuapp.com
